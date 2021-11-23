a8cfa44aeeb27d18f9c5a369b4806d6a280fb30f
MAMA Anas <anas.mama@hotmail.com>
2021-11-23 20:03:28 +0100

    ajout background palmier + hover sur deck

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,7 +1,9 @@
+import '../Deck.css'
+
 const Deck = () => {
     return (
         <div>
-            <img src='https://i.pinimg.com/originals/c5/b1/f3/c5b1f35d29702f1d9cb49f5760029603.png' alt='Dos de carte' style={{ height: '314px', widht: '226px' }}/>
+            <a href='a'><img className='deck' src='https://i.pinimg.com/originals/c5/b1/f3/c5b1f35d29702f1d9cb49f5760029603.png' alt='Dos de carte' style={{ height: '314px', widht: '226px' }}/></a>
         </div>
     );
 }
